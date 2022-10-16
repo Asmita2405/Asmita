262d7d10610a3d2bfc95fff4e181a148e78547d0
Asmita2405 <asmitashree143@gmail.com>
2022-10-16 18:07:21 +0530

    Entered in testing

--- Add.js
+++ Add.js
@@ -5,13 +5,14 @@ export default class Add extends React.Component
     super()
     this.x1=React.createRef()
     this.x2=React.createRef()
+    Add();
   }
   myfun(){
     //alert()
     console.log(this.x1);
     console.log(this.x2);
-    console.log(this.x1.current.value);
-    console.log(this.x2.current.value);
+    console.log(this.x1.current.value);//output on console
+    console.log(this.x2.current.value);//output on console
   }
   render(){
         return(
